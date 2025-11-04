ca9dd33bd5ec720fdaecc30c2df946b011e1a95c
AdityaP700 <adityaa32078@gmail.com>
2025-11-04 17:57:12 +0000

    added initial main command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod display;
+mod errors;
+mod jutsu;
+
+use cli::{Cli,Commands};
+use clap::Parser;
+
+#[tokio::main]
+async fn main()->Result<()> {
+    //it parses the command from the outside world
+    let cli = cli::Cli::parse();
+
+    //it decides which specialist to send the thing
+    match cli.command{
+     Commands::Unmask {mint_address}=>{
+        //we dispatch the unmask command to the jutsu module
+        jutsu::unmask::run(mint_address).await?;
+     }
+    }
+    Ok(())
 }
